2d33b52d48ec425919110646004cc68396b2f59a
Verma, Arun <arun.verma@rakuten.com>
2020-05-22 17:56:28 +0900

    add handler

---
+++ src/http/http_access_token.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vermaarun/bookstore_oauth-api/src/domain/access_token"
+	"net/http"
+	"strings"
+)
+
+type AccessTokenHandler interface {
+	GetById(c *gin.Context)
+}
+
+type accessTokenHandler struct {
+	service access_token.Service
+}
+
+func NewHandler(service access_token.Service) AccessTokenHandler {
+	return &accessTokenHandler{
+		service: service,
+	}
+}
+
+func (h *accessTokenHandler) GetById(c *gin.Context) {
+	accessTokenId := strings.TrimSpace(c.Param("access_token_id"))
+
+	accessToken, err := h.service.GetById(accessTokenId)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusOK, accessToken)
+}
